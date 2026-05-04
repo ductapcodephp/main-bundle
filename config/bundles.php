8d05d5c3da7f0230996c5f3ae1cb45f79a4b27a5
ductapcodephp <minhduc9a10dz@gmail.com>
2026-05-04 17:34:44 +0700

    [update]: route

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,12 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    AmzsCMS\MenuBundle\AmzsMenuBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    AmzsCMS\SettingBundle\AmzsSettingBundle::class => ['all' => true],
+    AmzsCMS\CoreBundle\AmzsCoreBundle::class => ['all' => true],
+    AmzsCMS\UserBundle\AmzsUserBundle::class => ['all' => true],
+    AmzsCMS\TopicBundle\AmzsTopicBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
